c44b6ed7fb0b7aedc51cf82c3d7f5409b6ca1545
warcraft14115 <goddeadbb@gmail.com>
2021-06-11 07:14:45 +0700

    sdffsdf

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-const incstr = require("incstr"),
-    classNames = {},
-    generateClassName = incstr.idGenerator({
-        alphabet: "abcdefghijklmnopqrstuvwxyz",
-    });
+
 module.exports = {
     poweredByHeader: !1,
     images: {
@@ -15,32 +11,4 @@ module.exports = {
     //    webpack5: true,
     // },
 
-    // webpack: (config, { dev }) => {
-    //     if (!dev) {
-    //         const MangleCssClassPlugin = require("mangle-css-class-webpack-plugin");
-    //         config.plugins.push(
-    //             new MangleCssClassPlugin({
-    //                 classNameRegExp: `(([a-zA-Z-:]*)[\\\\\\\\]*:)*([\\\\\\\\]*!)?tw-[a-zA-Z-]([a-zA-Z0-9-]*([\\\\\\\\]*(\\%|\\#|\\.|\\[|\\]))*)*`,
-    //                 //  ignorePrefixRegExp: "((hover|focus|active|disabled|visited|first|last|odd|even|group-hover|focus-within|xs|sm|md||lg|xl)(\\\\\\\\\\\\\\\\|\\\\)?:)*",
-
-    //                 classGenerator: (original, opts, context) => {
-    //                     if (classNames[original]) {
-    //                         return classNames[original];
-    //                     }
-
-    //                     let nextId;
-
-    //                     do {
-    //                         // Class name cannot start with a number.
-    //                         nextId = generateClassName();
-    //                     } while (/^[0-9_-]/.test(nextId));
-
-    //                     return classNames[original] = nextId;
-    //                 },
-    //                 //  log: true
-    //             })
-    //         );
-    //     }
-    //     return config;
-    // },
 };
